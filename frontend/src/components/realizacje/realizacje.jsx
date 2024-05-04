5290fc37dfe1f7d870514db4e3087c3d78dd289e
MikolajLesniczuk <119354331+MikolajLesniczuk@users.noreply.github.com>
2024-05-04 22:16:51 +0200

    doneee

--- frontend/src/components/realizacje/realizacje.jsx
+++ frontend/src/components/realizacje/realizacje.jsx
@@ -1,52 +1,155 @@
 import s from "./realizacje.module.scss";
-import one from "../../assets/beczki/1.jpg";
-import two from "../../assets/beczki/2.jpg";
-import three from "../../assets/beczki/3.jpg";
-import four from "../../assets/beczki/4.jpg";
+import dys from "../../assets/beczki/dystrybutor.jpg";
+import one from "../../assets/beczki/biala.jpg";
+import aaa from "../../assets/beczki/new.jpg";
+import two from "../../assets/beczki/czarna.jpg";
+import three from "../../assets/beczki/beka2.jpg";
+import four from "../../assets/beczki/beka.jpg";
 import five from "../../assets/beczki/5.jpg";
 import six from "../../assets/beczki/6.jpg";
 import seven from "../../assets/beczki/7.jpg";
-import eight from "../../assets/beczki/8.jpg";
-import nine from "../../assets/beczki/1.jpg";
-import eleven from "../../assets/beczki/2.jpg";
-import twelve from "../../assets/beczki/3.jpg";
-import fifteen from "../../assets/beczki/4.jpg";
-import sixteen from "../../assets/beczki/5.jpg";
-import seventeen from "../../assets/beczki/6.jpg";
-import eighteen from "../../assets/beczki/7.jpg";
+import eight from "../../assets/beczki/nowamala.jpg";
+import nine from "../../assets/beczki/9.jpg";
+import eleven from "../../assets/beczki/10.jpg";
+import twelve from "../../assets/beczki/11.jpg";
+import fifteen from "../../assets/beczki/12.jpg";
+import sixteen from "../../assets/beczki/15.jpg";
+import seventeen from "../../assets/beczki/1.jpg";
+import eighteen from "../../assets/beczki/2.jpg";
 import nineteen from "../../assets/beczki/8.jpg";
-import twenty from "../../assets/beczki/1.jpg";
-import twentythree from "../../assets/beczki/2.jpg";
-
+import twenty from "../../assets/beczki/nowa.jpg";
+import twentythree from "../../assets/beczki/4.jpg";
 export const Realizacje = () => {
   return (
     <div>
       <div className={s.opis}>
-        Nasz asortyment składa się z beczek o różnych pojemnościach. Posiadamy w
-        spzedaży beczki od 500L do 100.000 L
+        Nasz asortyment składa się ze zbiorników o różnych pojemnościach.
+        Posiadamy w spzedaży zbiorniki od 500L do 100.000 L
+        <p>Podane ceny są cenami do negocjacji</p>
+        <p>Możliwość załadunku i transportu każdego zbiornika</p>
       </div>
       <div className={s.positionphoto}>
-        <div className="gallery">
-          <img alt="something" className={s.photo} src={nineteen}></img>
-          <img alt="something" className={s.photo} src={two}></img>
+        <div className={s.gallery}>
+          <div className={s.box}>
+            <img alt="something" className={s.photo} src={dys}></img>
+            <p className={s.descphoto}>
+              Zbiornik stalowy dwupłaszczowy 13.000L
+            </p>
+            <p className={s.price}>13.900zł</p>
+          </div>
+          <div className={s.box}>
+            <img alt="something" className={s.photo} src={nineteen}></img>
+            <p className={s.descphoto}>Zbiornik dwupłaszczowy 100.000 L</p>
+            <p className={s.price}>42.000zł</p>
+          </div>
+          <div>
+            <img alt="something" className={s.photo} src={two}></img>
+            <p className={s.descphoto}>Zbiornik stalowy 50.000 L</p>
+            <p className={s.price}>23.900zł</p>
+          </div>{" "}
+          <div>
+            <img alt="something" className={s.photo} src={one}></img>
+            <p className={s.descphoto}>Zbiornik stalowy 100.000 L</p>
+            <p className={s.price}>69.000zł</p>
+          </div>{" "}
+          <div>
+            <img alt="something" className={s.photo} src={three}></img>
+            <p className={s.descphoto}>
+              Zbiornik stalowy dwupłaszczowy 40.000 L
+            </p>
+            <p className={s.price}>36.000zł</p>
+          </div>{" "}
+          <div>
+            <img alt="something" className={s.photo} src={four}></img>
+            <p className={s.descphoto}>
+              Zbiornik stalowy dwupłaszczowy 60.000L
+            </p>
+            <p className={s.price}>52.000zł</p>
+          </div>
+          <div>
+            <img alt="something" className={s.photo} src={aaa}></img>
 
-          <img alt="something" className={s.photo} src={one}></img>
-          <img alt="something" className={s.photo} src={three}></img>
+            <p className={s.descphoto}>Zbiornik stalowy 50.000 L</p>
+            <p className={s.price}>23.900zł</p>
+          </div>
+          <div>
+            <img alt="something" className={s.photo} src={five}></img>
+            <p className={s.descphoto}>Zbiornik stalowy 50.000 L</p>
+            <p className={s.price}>24.500 zł</p>
+          </div>{" "}
+          <div>
+            <img alt="something" className={s.photo} src={six}></img>
 
-          <img alt="something" className={s.photo} src={four}></img>
-          <img alt="something" className={s.photo} src={five}></img>
-          <img alt="something" className={s.photo} src={six}></img>
-          <img alt="something" className={s.photo} src={seven}></img>
-          <img alt="something" className={s.photo} src={eight}></img>
-          <img alt="something" className={s.photo} src={nine}></img>
-          <img alt="something" className={s.photo} src={eleven}></img>
-          <img alt="something" className={s.photo} src={twelve}></img>
-          <img alt="something" className={s.photo} src={fifteen}></img>
-          <img alt="something" className={s.photo} src={sixteen}></img>
-          <img alt="something" className={s.photo} src={seventeen}></img>
-          <img alt="something" className={s.photo} src={eighteen}></img>
-          <img alt="something" className={s.photo} src={twenty}></img>
-          <img alt="something" className={s.photo} src={twentythree}></img>
+            <p className={s.descphoto}>Zbiornik stalowy 50.000 L</p>
+            <p className={s.price}>24.500 zł</p>
+          </div>{" "}
+          <div>
+            <img alt="something" className={s.photo} src={seven}></img>
+            <p className={s.descphoto}>Zbiornik stalowy 50.000 L</p>
+            <p className={s.price}>24.500 zł</p>
+          </div>{" "}
+          <div>
+            <img alt="something" className={s.photo} src={eight}></img>
+
+            <p className={s.descphoto}>Zbiornik stalowy 3.000L</p>
+            <p className={s.price}>1.200 zł</p>
+          </div>{" "}
+          <div>
+            <img alt="something" className={s.photo} src={nine}></img>
+
+            <p className={s.descphoto}>
+              Zbiornik stalowy dwupłaszczowy 40.000 L
+            </p>
+            <p className={s.price}>35.000 zł</p>
+          </div>{" "}
+          <div>
+            <img alt="something" className={s.photo} src={eleven}></img>
+
+            <p className={s.descphoto}>Zbiornik stalowy 5.000 L</p>
+            <p className={s.price}>2.100 zł</p>
+          </div>{" "}
+          <div>
+            <img alt="something" className={s.photo} src={twelve}></img>
+
+            <p className={s.descphoto}>Zbiornik stalowy 10.000L</p>
+            <p className={s.price}>5.300 zł</p>
+          </div>
+          <div>
+            <img alt="something" className={s.photo} src={fifteen}></img>
+
+            <p className={s.descphoto}>Zbiornik stalowy 20.000L</p>
+            <p className={s.price}>4.900 zł</p>
+          </div>
+          <div>
+            <img alt="something" className={s.photo} src={sixteen}></img>
+
+            <p className={s.descphoto}>Zbiornik stalowy 50.000 L</p>
+            <p className={s.price}>24.500 zł</p>
+          </div>{" "}
+          <div>
+            <img alt="something" className={s.photo} src={seventeen}></img>
+
+            <p className={s.descphoto}>Zbiornik stalowy 50.000 L</p>
+            <p className={s.price}>24.500 zł</p>
+          </div>{" "}
+          <div>
+            <img alt="something" className={s.photo} src={eighteen}></img>
+
+            <p className={s.descphoto}>Zbiornik stalowy50.000 L</p>
+            <p className={s.price}>23.900 zł</p>
+          </div>{" "}
+          <div>
+            <img alt="something" className={s.photo} src={twenty}></img>
+
+            <p className={s.descphoto}>Zbiornik stalowy 50.000 L</p>
+            <p className={s.price}>23.900 zł</p>
+          </div>{" "}
+          <div>
+            <img alt="something" className={s.photo} src={twentythree}></img>
+
+            <p className={s.descphoto}>Zbiornik stalowy 50.000 L</p>
+            <p className={s.price}>23.900 zł</p>
+          </div>
         </div>
       </div>
     </div>
