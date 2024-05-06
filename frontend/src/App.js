90502a215d26abb3efc41febc5e014057edb9cf8
MikolajLesniczuk <119354331+MikolajLesniczuk@users.noreply.github.com>
2024-05-06 22:10:58 +0200

    wip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import s from "./app.module.scss";
 import { Footer } from "./components/footer/footer";
-import { Route, Routes } from "react-router-dom";
 import { Mainpage } from "./components/mainpage/mainpage";
-
 import { Onas } from "./components/onas/onas";
 import { Realizacje } from "./components/realizacje/realizacje";
 import { Navigation } from "./components/navigation/navigation";
@@ -10,6 +11,23 @@ import { Kontakt } from "./components/kontakt/kontakt";
 import { Transport } from "./components/transport/transport";
 
 export const App = () => {
+  const location = useLocation();
+
+  // Funkcja do zapisywania odwiedzonych ścieżek w localStorage
+  const saveVisitedRoutes = (route) => {
+    const visitedRoutes =
+      JSON.parse(localStorage.getItem("visitedRoutes")) || [];
+    if (!visitedRoutes.includes(route)) {
+      visitedRoutes.push(route);
+      localStorage.setItem("visitedRoutes", JSON.stringify(visitedRoutes));
+    }
+  };
+
+  // Wywołanie funkcji zapisującej odwiedzoną ścieżkę przy zmianie lokalizacji
+  useEffect(() => {
+    saveVisitedRoutes(location.pathname);
+  }, [location]);
+
   return (
     <>
       <Navigation />
@@ -18,11 +36,9 @@ export const App = () => {
         <Routes>
           <Route path="*" element={<Mainpage />} />
           <Route path="/" element={<Mainpage />} />
-
           <Route path="/onas" element={<Onas />} />
           <Route path="/oferta" element={<Realizacje />} />
           <Route path="/transport" element={<Transport />} />
-
           <Route path="/kontakt" element={<Kontakt />} />
         </Routes>
       </div>
